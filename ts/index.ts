82577bdf64acb8482b27d64933a4098ca6d61e9f
Philipp Kunz <kunzphp@gmail.com>
2015-12-06 03:08:29 +0100

    initial

---
+++ ts/index.ts
@@ -0,0 +1,7 @@
+/// <reference path="typings/tsd.d.ts" />
+var through = require("through2");
+module.exports = () => {
+    return through.obj(function(file, enc, cb) {
+        return cb(null, file); //run callback function to signal end of plugin process.
+    });
+};
